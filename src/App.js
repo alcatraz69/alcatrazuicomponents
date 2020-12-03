3cceb341177620bc76a77a8bf7014d6d419d26f3
Prem Kantikar <48849830+alcatraz69@users.noreply.github.com>
2020-12-03 17:05:06 +0530

    added wifi animation

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Wifi from './components/wifi/wifi';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <h1>Hello Ultimate</h1>
+        <Wifi/>
       </header>
     </div>
   );
---
+++ src/components/wifi/wifi.js
@@ -0,0 +1,22 @@
+
+import './wifi.css';
+
+function Wifi() {
+  return (
+    <div>
+    <p className="glow">glow me</p>
+    <div id="router">
+    <div id="body">
+      <div id="wifi">
+      <div className="signal"></div>
+        <div className="signal"></div>
+        <div className="signal"></div>
+        <div className="signal"></div>
+      </div>
+    </div>
+  </div>
+  </div>
+  );
+}
+
+export default Wifi;
