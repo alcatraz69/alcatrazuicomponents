8f2b24d2db65a1703b7dd923719db99d37a35cd4
Prem Kantikar <48849830+alcatraz69@users.noreply.github.com>
2020-12-08 00:26:38 +0530

    chore-added social icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Wifi from './components/wifi/wifi';
+import Socialicons from './components/socialsites/socialicons';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Wifi/>
+        <Socialicons/>
       </header>
     </div>
   );
