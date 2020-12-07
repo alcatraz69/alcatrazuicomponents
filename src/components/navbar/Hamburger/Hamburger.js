86054063ab77d46a52a0adae0fa15240b35e7e49
Muthu Kumaran <61352185+KumaranSahi@users.noreply.github.com>
2020-12-07 23:50:18 +0530

    chore-added hamburger icon

--- src/components/navbar/Hamburger/Hamburger.js
+++ src/components/navbar/Hamburger/Hamburger.js
@@ -1,12 +1,31 @@
-import react from 'react';
 import classes from './Hamburger.module.css';
+import React,{ useState } from 'react';
 
-const hamburger=(props)=>{
+const hamburger=React.memo((props)=>{
+    const [iconClicked,seticonClicked]=useState(false);
+    
+    let lineOne=classes.hamburgerLine;
+    let lineTwo=classes.hamburgerLine;
+    let lineThree=classes.hamburgerLine;
+
+    let hamburgerClicked=(event)=>{
+        let currentStatus=iconClicked;
+        seticonClicked(!currentStatus);
+    }
+
+    if(iconClicked){
+        lineOne=`${classes.hamburgerLine} ${classes.lineTop}`
+        lineTwo=`${classes.hamburgerLine} ${classes.lineMiddle}`
+        lineThree=`${classes.hamburgerLine} ${classes.lineBottom}`
+    }
+
     return(
-        <div className={classes.hamburgerContainer}>
-            
+        <div className={classes.hamburgerContainer} onClick={event=>hamburgerClicked(event)}>
+            <div className={lineOne}></div>
+            <div className={lineTwo}></div>
+            <div className={lineThree}></div>
         </div>
     )
-}
+})
 
-export default hamburger
+export default hamburger;
